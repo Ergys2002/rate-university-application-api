6171f11c55121c5c5da34878ae05328cd0b2fa2b
Ergys <ergysxhaollari02@gmail.com>
2023-12-01 23:42:47 +0100

    User Entity Jwt Authentication And Spring Security Configured

---
+++ src/main/java/com/app/rateuniversityapplicationapi/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.app.rateuniversityapplicationapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
