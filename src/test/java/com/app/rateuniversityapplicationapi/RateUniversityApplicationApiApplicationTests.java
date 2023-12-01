b9359df75021de6cc97740a99c737d375b028e75
Ergys <ergysxhaollari02@gmail.com>
2023-12-01 10:38:31 +0100

    initial commit empty project

---
+++ src/test/java/com/app/rateuniversityapplicationapi/RateUniversityApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.rateuniversityapplicationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateUniversityApplicationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
