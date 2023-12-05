41cf8c42844e5c120e84a21af17fba651eb5d5e6
Maksimo <MaksimoRamaj@Gmail.com>
2023-12-05 16:38:19 +0100

    Implemented 2 test methods and made some code refactoring

--- src/main/java/com/app/rateuniversityapplicationapi/controller/CourseController.java
+++ src/main/java/com/app/rateuniversityapplicationapi/controller/CourseController.java
@@ -4,10 +4,7 @@ import com.app.rateuniversityapplicationapi.entity.Course;
 import com.app.rateuniversityapplicationapi.service.CourseService;
 import com.app.rateuniversityapplicationapi.service.ICourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +21,7 @@ public class CourseController {
     }
 
     @GetMapping("/{course-name}")
-    public List<Course> getCoursesByName(@PathVariable String courseName){
+    public List<Course> getCoursesByName(@PathVariable("course-name") String courseName){
         return courseService.getCoursesByName(courseName);
     }
 
